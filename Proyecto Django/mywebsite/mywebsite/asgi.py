921e03c6574f0f2478125edb5497ef26e78a7847
Lorena Rojas <lrojas@FACTORYHKA.LOCAL>
2026-02-13 14:57:33 -0400

    Desarrollo de nuestra interfaz web

--- Proyecto Django/mywebsite/mywebsite/asgi.py
+++ Proyecto Django/mywebsite/mywebsite/asgi.py
@@ -1,12 +1,10 @@
 import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
-# application = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    'http':get_asgi_application()
+    'http':get_asgi_application(),
 })
--- Proyecto Django/mywebsite/mywebsite/settings.py
+++ Proyecto Django/mywebsite/mywebsite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
