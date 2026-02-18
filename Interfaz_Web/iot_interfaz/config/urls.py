bd16ecdfe4762b40ff992f80c575d88d4452de3c
Lorena Rojas <lrojas@FACTORYHKA.LOCAL>
2026-02-18 09:29:54 -0400

    Registro e inicio de dashboard

--- Interfaz_Web/iot_interfaz/config/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from impresoras.views import MiLogin #Importamos views
-
-urlpatterns = [
-    path('login/', MiLogin.as_view(), name='login'),
-]
--- Interfaz_Web/iot_interfaz/impresoras/views.py
+++ Interfaz_Web/iot_interfaz/impresoras/views.py
@@ -1,4 +1,11 @@
 from django.contrib.auth.views import LoginView
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 class MiLogin(LoginView):
-    template_name = "login.html" #El HTML que queremos mostrar
+    template_name = "login.html"
+
+@login_required # Solo para usuarios loggeados
+def dashboard_view(request):
+    # Mas adelante enviaremos datos a BD
+    return render(request, 'dashboard.html')
