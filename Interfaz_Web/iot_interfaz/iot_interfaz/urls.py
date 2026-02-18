818275d32f50cf7b157a4cdff793af8e020f2ae9
Lorena Rojas <lrojas@FACTORYHKA.LOCAL>
2026-02-18 10:02:12 -0400

    Creacion de superusuario y usuario comun

--- Interfaz_Web/iot_interfaz/iot_interfaz/urls.py
+++ Interfaz_Web/iot_interfaz/iot_interfaz/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from impresoras.views import MiLogin, dashboard_view #Importamos views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('login/', MiLogin.as_view(), name='login'),
     path('dashboard/', dashboard_view, name='dashboard'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
